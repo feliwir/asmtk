09a4a00054bf2ceaa9bb11828c07fc463acdac87
feliwir <stephan.vedder@gmail.com>
2016-10-30 19:58:02 +0100

    Added asmlinker files

---
+++ src/asmtk/asmlinker.h
@@ -0,0 +1,22 @@
+// [AsmTk]
+// Assembler toolkit based on AsmJit.
+//
+// [License]
+// Zlib - See LICENSE.md file in the package.
+
+// [Guard]
+#ifndef ASMTK_ASMLINKER_H
+#define ASMTK_ASMLINKER_H
+
+namespace asmtk {
+
+using asmjit::Error
+
+class AsmLinker {
+
+};
+
+}
+
+// [Guard]
+#endif // ASMTK_ASMLINKER_H
